8e7771d4990da1f76082169a465763dc98818ed6
Shashank Naik <shashanknaik808@gmail.com>
2023-11-09 12:02:56 +0530

    Implementing and Importing components for header, products, signup, and cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import Header from './components/header/Header';
+
+import Products from './components/products/Products';
+import SignUp from './components/signup/SignUp';
+import Cart from './components/cart/Cart';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [cartItems, setcartItems] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
