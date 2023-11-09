6bd34584d79799da4ebcb8c1c58a4d4a68fac554
Shashank Naik <shashanknaik808@gmail.com>
2023-11-09 13:06:34 +0530

    Enhance Header component with navigation links and cart icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
-import Header from './components/header/Header';
+import Header from './components/header/Header.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Products from './components/products/Products';
-import SignUp from './components/signup/SignUp';
-import Cart from './components/cart/Cart';
-// import { useState } from 'react';
+import Products from './components/products/Products.jsx';
+import SignUp from './components/signup/SignUp.jsx';
+import Cart from './components/cart/Cart.jsx';
+import { useState } from 'react';
 
 
 function App() {
 
-  // const [cartItems, setcartItems] = useState([]);
+  const [cartItems, setcartItems] = useState([]);
 
   return (
     <div className="App">
       <BrowserRouter>
-        <Header cartItems={null} />
+        <Header cartItems={cartItems} />
         <Routes>
           <Route exact path="/" element={<Products />} />
           <Route exact path="/signup" element={<SignUp />} />
-          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/cart" element={<Cart cartItems={cartItems} />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Header() {
+function Header(props) {
     return (
-        <div>Header</div>
+        <header className='header'>
+            <div>
+                <h1>
+                    <Link to='/' className='logo'>
+                        Fruit Kart
+                    </Link>
+                </h1>
+            </div>
+
+            <div className='header-links'>
+                <ul>
+                    <li>
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <Link to="/signup">
+                            SignUp
+                        </Link>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <Link to="/cart" className='cart'>
+                            <i className='' />
+                            <span className='cart-length'>
+                                {props.cartItems.length}
+                            </span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
     )
 }
 
