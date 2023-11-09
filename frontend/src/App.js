b7fdadbf7dc3e6849a0aa8aaa0a0f41d03e3d6a6
Shashank Naik <shashanknaik808@gmail.com>
2023-11-09 12:36:34 +0530

    React-Router-Dom Running Successfull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Products from './components/products/Products';
 import SignUp from './components/signup/SignUp';
 import Cart from './components/cart/Cart';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 
 function App() {
 
-  const [cartItems, setcartItems] = useState([]);
+  // const [cartItems, setcartItems] = useState([]);
 
   return (
     <div className="App">
       <BrowserRouter>
-        <Header cartItems />
+        <Header cartItems={null} />
         <Routes>
-          <Route exact path="/" element={Products} />
-          <Route exact path="/signup" element={SignUp} />
-          <Route exact path="/cart" element={Cart} />
+          <Route exact path="/" element={<Products />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </div>
