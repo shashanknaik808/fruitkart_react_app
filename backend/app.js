30cba01e197268f4bc6d62db4f12ba6996176459
Shashank Naik <shashanknaik808@gmail.com>
2023-11-25 12:30:55 +0530

    Backend Server Ready

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,10 @@
-<div>
-    Hello World
-</div>
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3300, () => {
+    console.log("Backend Application Running on 3300");
+});
