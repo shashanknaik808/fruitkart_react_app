3aa1d1e4ba7d1481ec24012a13bdabf38739fa1b
Shashank Naik <shashanknaik808@gmail.com>
2023-11-25 18:06:40 +0530

    Enhance handleRemoveProduct function for better handling of item removal and quantity adjustments.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,6 +27,25 @@ function App() {
         }
     }
 
+    function handleRemoveProduct(product) {
+        const productExist = cartItems.find(item => {
+            return item.id === product.id;
+        });
+
+        if (productExist.quantity === 1) {
+            setCartItems(cartItems.filter(item => {
+                return item.id !== product.id;
+            }));
+        }
+        else {
+            setCartItems(cartItems.map(item => {
+                return item.id === product.id ?
+                    { ...productExist, quantity: productExist.quantity - 1 } :
+                    item
+            }))
+        }
+    }
+
 
     return (
         <div className='App'>
