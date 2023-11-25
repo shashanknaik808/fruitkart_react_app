705694b88e8bf2588286627256b6fb7835e968ba
Shashank Naik <shashanknaik808@gmail.com>
2023-11-25 15:17:32 +0530

    Refactored product rendering based on the 'flag' condition.

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useEffect, useState } from 'react';
 
 function Products(props) {
@@ -6,7 +5,7 @@ function Products(props) {
     const [products, setProducts] = useState({ flag: false, productList: null });
 
     async function getData() {
-        let backend_url = 'http://localhost:3300/product';
+        let backend_url = 'http://localhost:3300/products';
         let response = await fetch(backend_url);
         let responseData = await response.json();
         setProducts({ flag: true, productList: responseData['productItems'] });
@@ -14,9 +13,38 @@ function Products(props) {
 
     useEffect(() => { getData() }, []);
 
-    return (
-        <div>Products</div>
-    )
+    return ((products.flag) ?
+        <div className='products-container'>
+            <div className='products'>
+                {products.productList.map(item => {
+                    return (
+                        <div className='card' key={item.name}>
+                            <div>
+                                <img className='product-image' src={item.image} alt={item.image} />
+                            </div>
+
+                            <div>
+                                <h2 className='product-name'>
+                                    {item.name}
+                                </h2>
+                            </div>
+
+                            <div className='product-price'>
+                                Rs.{item.price}
+                            </div>
+
+                            <div>
+                                <button className='product-add-button'
+                                    onClick={() => props.handleAddProduct(item)}>
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div> : <div><h1>Loading..!!</h1></div>
+    );
 }
 
 export default Products;
