ed21012455615a8a58510ae42f1a9b784d0ca87e
Shashank Naik <shashanknaik808@gmail.com>
2023-11-25 13:28:08 +0530

    Setting Up the Routing for Header, Products, SignUp, Cart Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Products from './components/products/Products';
+import SignUp from './components/signup/SignUp';
+import Cart from './components/cart/Cart';
 
 function App() {
 
@@ -8,10 +13,15 @@ function App() {
     return (
         <div className='App'>
             <BrowserRouter>
-            
+                <Header />
+                <Routes>
+                    <Route exact path="/" element={<Products />} />
+                    <Route exact path="/signup" element={<SignUp />} />
+                    <Route exact path="/cart" element={<Cart />} />
+                </Routes>
             </BrowserRouter>
         </div>
-    )
+    );
 }
 
 export default App;
