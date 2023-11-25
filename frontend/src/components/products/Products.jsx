34de8e2fd81be9147c142793d1c6fba01a440aab
Shashank Naik <shashanknaik808@gmail.com>
2023-11-25 15:13:19 +0530

    Implement Products component to fetch and display product data from API

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Products() {
+function Products(props) {
+
+    const [products, setProducts] = useState({ flag: false, productList: null });
+
+    async function getData() {
+        let backend_url = 'http://localhost:3300/product';
+        let response = await fetch(backend_url);
+        let responseData = await response.json();
+        setProducts({ flag: true, productList: responseData['productItems'] });
+    }
+
+    useEffect(() => { getData() }, []);
+
     return (
         <div>Products</div>
     )
