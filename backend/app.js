313c68554010767a9e5dcde7cb0eb11043c574ef
Shashank Naik <shashanknaik808@gmail.com>
2023-11-25 12:56:02 +0530

    Added ProductsRouter for '/products' Endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const productsRouter = require('./routes/productsRoute.js');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(productsRouter);
 
 app.listen(3300, () => {
     console.log("Backend Application Running on 3300");
